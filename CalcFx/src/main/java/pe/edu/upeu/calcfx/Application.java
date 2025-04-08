a968a61670ef834ff1860e0302c2f4f623d884dd
hrems-dev <hrems077@gmail.com>
2025-04-08 11:27:41 -0500

    avance en clase

--- CalcFx/src/main/java/pe/edu/upeu/calcfx/Application.java
+++ CalcFx/src/main/java/pe/edu/upeu/calcfx/Application.java
@@ -1,4 +1,10 @@
 package pe.edu.upeu.calcfx;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 public class Application {
+    public static void main(String[] args) {
+        CalcFxApplication.main(args);
+    }
 }
