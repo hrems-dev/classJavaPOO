ab1c7061c1145a3b2af94cf4f8cc9c4486548b25
hrems-dev <hrems077@gmail.com>
2025-04-11 11:03:36 -0500

    mi comentario agregado

--- CalcFx/src/main/java/pe/edu/upeu/calcfx/CalcFxApplication.java
+++ CalcFx/src/main/java/pe/edu/upeu/calcfx/CalcFxApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalcFxApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // valore de cambio
 		SpringApplication.run(CalcFxApplication.class, args);
 	}
 
