67c031993a9312b51ca5e217a290bd2459ca4a91
hrems-dev <hrems077@gmail.com>
2025-04-11 11:06:54 -0500

    mi cambio

--- CalcFx/src/main/java/pe/edu/upeu/calcfx/CalcFxApplication.java
+++ CalcFx/src/main/java/pe/edu/upeu/calcfx/CalcFxApplication.java
@@ -1,13 +1,19 @@
 package pe.edu.upeu.calcfx;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalcFxApplication {
+public class CalcFxApplication extends Application {
 
 	public static void main(String[] args) { // valore de cambio
 		SpringApplication.run(CalcFxApplication.class, args);
 	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+
+	}
 }
